fd500c60d09576c8359b75acfd8d3503d95cac12
Marcel Bro <marcel.broz@gmail.com>
2024-04-05 23:40:34 +0100

    Kotlin and Compose updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.compose") version("1.5.12")
+    id("org.jetbrains.compose") version("1.6.1")
 }
 
 group = "net.solvetheriddle.cardgame"
