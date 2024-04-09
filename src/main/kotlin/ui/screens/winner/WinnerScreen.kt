cf02a9b4bc89ab3db71ceb17976ae487482fcdef
Marcel Bro <marcel.broz@gmail.com>
2024-04-09 01:29:16 +0100

    Winner screen UI updated

--- src/main/kotlin/ui/screens/winner/WinnerScreen.kt
+++ src/main/kotlin/ui/screens/winner/WinnerScreen.kt
@@ -1,38 +1,56 @@
 package ui.screens.winner
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun WinnerScreen(winnerName: String, playAgain: () -> Unit) {
-    Column(
+    Box(
         modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "The winner is",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+        Image(
+            painterResource("img/winner_background.png"),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
-        Spacer(Modifier.height(16.dp))
-        Text(
-            text = winnerName,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            fontSize = 80.sp,
-        )
-        Spacer(Modifier.height(72.dp))
-        Button(
-            onClick = { playAgain() },
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text("Play again!")
+            Text(
+                text = "The winner is",
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(Modifier.height(16.dp))
+            Text(
+                text = winnerName.uppercase(),
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                fontSize = 80.sp,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Spacer(Modifier.height(40.dp))
+            Button(
+                onClick = { playAgain() },
+            ) {
+                Text("PLAY AGAIN")
+            }
         }
     }
-
 }
