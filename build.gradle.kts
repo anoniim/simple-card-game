76b68799c5b0c7daa4fbd765dd6c47ba816283ef
Marcel Bro <marcel.broz@gmail.com>
2024-05-11 23:03:27 +0100

    Print file structure of packaged application in GitHub Actions * Debug print commented out

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,14 @@ compose.desktop {
     }
 }
 
+tasks.register<Copy>("setExecutablePermission") {
+    doLast {
+        val executableFile = file("${layout.buildDirectory}/compose/binaries/main/app/simple-card-game.app/Contents/MacOS/simple-card-game")
+        executableFile.setExecutable(true, false)
+    }
+    dependsOn("packageMacosX64AppRelease")
+}
+
 repositories {
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
