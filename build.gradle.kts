634d6089fcc2613f0595a4ebd8cfe9a3c6a6bf0d
Marcel Bro <marcel.broz@gmail.com>
2024-05-25 07:18:30 +0200

    Test dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,4 +41,9 @@ dependencies {
     val koinVersion = "3.5.3"
     implementation(platform("io.insert-koin:koin-bom:$koinVersion"))
     implementation("io.insert-koin:koin-core")
+
+//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+//    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+//    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
