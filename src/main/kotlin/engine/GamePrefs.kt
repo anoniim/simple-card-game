bb75640b77b471cb03c9c9fcde668c3912d35f41
Marcel Bro <marcel.broz@gmail.com>
2024-05-25 07:18:02 +0200

    Leaderboard can be saved

--- src/main/kotlin/engine/GamePrefs.kt
+++ src/main/kotlin/engine/GamePrefs.kt
@@ -1,8 +1,10 @@
 package engine
 
+import engine.rating.Leaderboard
 import java.util.prefs.Preferences
 
 private const val PLAYER_NAME = "playerName"
+private const val LEADERBOARD = "leaderboard"
 
 class GamePrefs {
     private val prefs: Preferences = Preferences.userRoot().node(this::class.java.name)
@@ -10,4 +12,15 @@ class GamePrefs {
     fun setPlayerName(playerName: String) = prefs.put(PLAYER_NAME, playerName )
 
     fun getPlayerName(): String = prefs.get(PLAYER_NAME, "")
+
+    fun getLeaderboard(): Leaderboard {
+        val serializedLeaderboard = prefs.get(LEADERBOARD, "")
+        println("serialized: $serializedLeaderboard")
+        return Leaderboard.deserialize(serializedLeaderboard)
+    }
+
+    fun updateLeaderboard(newLeaderboard: Leaderboard) {
+        prefs.put(LEADERBOARD, newLeaderboard.serialize())
+        println("deserialized: ${newLeaderboard.serialize()}")
+    }
 }
