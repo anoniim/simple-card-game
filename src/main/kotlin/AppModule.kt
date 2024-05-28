aa22600b8bc54e59adc6be94f128a98b97b5a2dc
Marcel Bro <marcel.broz@gmail.com>
2024-05-28 09:01:30 +0200

    DI factory created for ELO Rating System

--- src/main/kotlin/AppModule.kt
+++ src/main/kotlin/AppModule.kt
@@ -3,24 +3,26 @@ import engine.GamePrefs
 import engine.GameSettings
 import engine.player.PlayerFactory
 import engine.rating.EloRatingSystem
-import engine.rating.Leaderboard
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val appModule = module {
+
     singleOf(::GamePrefs)
     single { GameSettings.DEFAULT }
     singleOf(::PlayerFactory)
 
     factory {
-    val settings = get<GameSettings>()
+        val settings = get<GameSettings>()
         CardDeck(settings.numOfCardDecks)
+    }
+    factory {
+        val prefs = get<GamePrefs>()
+        EloRatingSystem(prefs.getLeaderboard())
     }
     factory { (playerName: String) ->
         val playerFactory = get<PlayerFactory>()
         val players = playerFactory.createPlayers(playerName)
-        val prefs = get<GamePrefs>()
-        val ratingSystem = EloRatingSystem(prefs.getLeaderboard())
-        GameEngine(players, get(), get(), ratingSystem)
+        GameEngine(players, get(), get(), get())
     }
 }
