7e8a19a77b947164f344db9d8e9ec41002760306
Marcel Bro <marcel.broz@gmail.com>
2024-06-08 23:53:36 +0100

    New consolidated game state introduced for ML training

--- ml-training/src/main/kotlin/Main.kt
+++ ml-training/src/main/kotlin/Main.kt
@@ -1,22 +1,42 @@
 package net.solvetheriddle.cardgame
 
+import GameEndState
 import GameEngine
+import SpeedMode
+import engine.Card
 import engine.CardDeck
 import engine.GameDifficulty
 import engine.GameSettings
+import engine.player.Player
 import engine.player.PlayerFactory
 import engine.rating.EloRatingSystem
 import engine.rating.Leaderboard
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import mocks.NoOpSoundPlayer
 
 fun main() {
     println("Let's do some ML training!")
     val game = createNewGameEngine("CP1")
-    GlobalScope.launch {
+    runBlocking {
+        val cardFlow = game.card
+        val playersFlow = game.players
+        val gameEndStateFlow = game.gameEndState
+        val gameStateFlow = combine(cardFlow, playersFlow, gameEndStateFlow) { card, players, gameEndState ->
+            GameState(players, card, gameEndState)
+        }
+        println("Setting collection")
+        launch {
+            gameStateFlow.collect { gameState ->
+                println("Game state: $gameState")
+            }
+        }
+        println("Starting game")
         game.startGame()
+
     }
+    println("done")
 }
 
 private fun createNewGameEngine(modelName: String): GameEngine {
@@ -25,5 +45,12 @@ private fun createNewGameEngine(modelName: String): GameEngine {
     val ratingSystem = EloRatingSystem(Leaderboard(emptyMap()))
     val sounds = Sounds(NoOpSoundPlayer())
     val players = PlayerFactory(settings).createPlayers(modelName)
-    return GameEngine(players, cardDeck, settings, ratingSystem, sounds)
+    return GameEngine(players, cardDeck, settings, ratingSystem, sounds,
+        speedMode = SpeedMode.FAST)
 }
+
+private data class GameState(
+    val players: List<Player>,
+    val card: Card?,
+    val gameEndState: GameEndState?
+)
