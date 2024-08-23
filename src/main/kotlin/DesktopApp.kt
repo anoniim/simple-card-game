98c4f1a67dc6f4f959ec5b2404663030c47f7358
Marcel Bro <marcel.broz@gmail.com>
2024-08-24 00:15:39 +0100

    Firebase config loaded from local properties

--- src/main/kotlin/DesktopApp.kt
+++ src/main/kotlin/DesktopApp.kt
@@ -9,6 +9,9 @@ import com.google.firebase.FirebasePlatform
 import com.google.firebase.initialize
 import org.koin.core.context.startKoin
 import ui.AppComposable
+import java.io.File
+import java.io.FileInputStream
+import java.util.Properties
 
 
 fun main() = application {
@@ -27,8 +30,15 @@ fun main() = application {
 
 fun initializeFirebase() {
     initializeFirebasePlatform()
+
+    val localProperties = loadProperties()
     val options = FirebaseOptions.Builder()
+        .setProjectId(localProperties.getProperty("firebase.projectId"))
+        .setApiKey(localProperties.getProperty("firebase.apiKey"))
+        .setApplicationId(localProperties.getProperty("firebase.appId"))
+        .setStorageBucket(localProperties.getProperty("firebase.storageBucket"))
         .build()
+
     Firebase.initialize(Application(), options)
 }
 
@@ -50,4 +60,26 @@ private fun initializeFirebasePlatform() {
             storage[key] = value
         }
     })
+}
+
+@Suppress("SENSELESS_COMPARISON") // Incorrect warning
+fun findFile(fileName: String): File? {
+    var currentDir = File(".").absoluteFile // Start from the current directory
+    do {
+        val file = File(currentDir, fileName)
+        if (file.exists()) return file
+        currentDir = currentDir.parentFile // Move up one directory
+    } while (currentDir != null)
+    return null
+}
+
+private fun loadProperties(): Properties {
+    val localProperties = Properties()
+    val localPropertiesFile = findFile("local.properties")
+    if (localPropertiesFile != null && localPropertiesFile.exists()) {
+        localProperties.load(FileInputStream(localPropertiesFile))
+    } else {
+        println("local.properties file not found")
+    }
+    return localProperties
 }
